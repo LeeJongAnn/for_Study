1c341188a9c6f6107a90d9ccefe9a84e28e8cea7
dlwhddksiq <dlwhddksiq@naver.com>
2022-04-15 21:24:58 +0900

    회원가입 값이 받아지는지 확인 및 리드미 수정

---
+++ src/main/resources/static/js/user.js
@@ -0,0 +1,19 @@
+let index = {
+		init: function(){
+			$("#btn-save").on("click", ()=>{ // function(){} , ()=>{} this를 바인딩하기 위해서!!
+				this.save();
+			});
+		},
+
+		save: function(){
+//			alert('user의 save함수 호출됨');
+			let data = {
+                username : $("#username").val(),
+                password : $("#password").val(),
+                email : $("#email").val()
+			}
+			console.log(data)
+			}
+}
+
+index.init()
