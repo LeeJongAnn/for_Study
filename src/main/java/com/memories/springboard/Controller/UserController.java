ee7f5c9d891888bf9e36c85b9a995233af52b272
dlwhddksiq <dlwhddksiq@naver.com>
2022-04-17 10:50:10 +0900

    브랜치 나누고 index 페이지에 가입한 아이디 나오도록 작성

--- src/main/java/com/memories/springboard/Controller/UserController.java
+++ src/main/java/com/memories/springboard/Controller/UserController.java
@@ -1,15 +1,28 @@
 package com.memories.springboard.Controller;
 
+import com.memories.springboard.Entity.User;
+import com.memories.springboard.UserService.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Controller
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/index")
-    public String firstviewPage(){
+    public String firstviewPage(Model model){
+
+        List<User> listAll= userService.showAll();
+        model.addAttribute("List",listAll);
         return "index";
+
     }
 
     @GetMapping("/join_form")
