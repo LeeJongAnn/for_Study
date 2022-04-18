76c325e5f2feaa3162e9628180771192da92d31c
dlwhddksiq <dlwhddksiq@naver.com>
2022-04-18 15:30:33 +0900

    스프링 시큐리티 로그인 작성
    로그인 및 회원가입 되는지 확인
    
    히스토리 추가

--- src/main/java/com/memories/springboard/Controller/UserController.java
+++ src/main/java/com/memories/springboard/Controller/UserController.java
@@ -2,7 +2,9 @@ package com.memories.springboard.Controller;
 
 import com.memories.springboard.Entity.User;
 import com.memories.springboard.UserService.UserService;
+import com.memories.springboard.config.PrincipalDetail;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,14 +19,15 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/SpringBoard")
-    public String viewHomePage() {
+    public String viewHomePage(@AuthenticationPrincipal PrincipalDetail principal) {
+        System.out.println(principal.getUsername());
         return "Home";
     }
 
 
     @GetMapping("/index")
-    public String firstviewPage(Model model) {
-
+    public String firstviewPage(@AuthenticationPrincipal PrincipalDetail principal,Model model) {
+        System.out.println(principal.getUsername());
         List<User> listAll = userService.showAll();
         model.addAttribute("List", listAll);
         return "index";
@@ -40,4 +43,6 @@ public class UserController {
     public String loginviewPage() {
         return "login_form";
     }
+
+
 }
---
+++ src/main/java/com/memories/springboard/config/PrincipalDetail.java
@@ -0,0 +1,61 @@
+package com.memories.springboard.config;
+
+import com.memories.springboard.Entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class PrincipalDetail implements UserDetails {
+
+    private User user;
+
+    public PrincipalDetail(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        Collection<GrantedAuthority> collector = new ArrayList<>();
+        collector.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return "ROLE_" + user.getRole();
+            }
+        });
+
+        return collector;
+    }
+}
