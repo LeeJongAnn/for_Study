c8d06000da18fc0ada6d01ee4f2b4f9af4c18d72
dlwhddksiq <dlwhddksiq@naver.com>
2022-04-21 10:41:38 +0900

    게시글 작성 후 페이지 오류 문제 수정

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -19,7 +19,7 @@ let index = {
             				dataType: "json"
             			}).done(function(resp){
             				alert("글쓰기가 완료되었습니다.");
-            				location.href = "/SpringBoard/board/view";
+            				location.href = "/SpringBoard/board/view/1";
             			}).fail(function(error){
             				alert(JSON.stringify(error));
             			});
